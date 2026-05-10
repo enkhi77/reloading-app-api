23ab029b339d8b6f8449dcdd1656e482276f931a
Wesley Song <wes.song@gmail.com>
2026-05-09 22:42:25 -0600

    add bullet model

---
+++ ReloadingAppWebApi/Models/Bullet.cs
@@ -0,0 +1,12 @@
+﻿namespace ReloadingAppWebApi.Models;
+
+public partial class Bullet
+{
+    public Guid Id { get; set; }
+    public string Caliber { get; set; }
+    public DateOnly? DatePurchased { get; set; }
+    public decimal Diameter { get; set; }
+    public string Lot { get; set; }
+    public decimal Price { get; set; }
+    public int Weight { get; set; }
+}
